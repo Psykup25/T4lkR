83f349d33b8b47138548d22226f19cc6b143bb08
Psykup25 <romg25@gmail.com>
2025-07-09 13:52:47 +0200

    Deuxieme commit

---
+++ client/src/public/page/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+  standalone: true,
+})
+export class Login {
+
+}
